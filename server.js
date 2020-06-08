0ce91fc872b3c1d3c09533cccf51090f8c966caa
Muhammad Ali Kazmi <ali.smak099@gmail.com>
2020-06-08 09:13:12 +0500

    Add sms verification

--- server.js
+++ server.js
@@ -4,26 +4,42 @@ const path = require('path');
 const compression = require('compression');
 const enforce = require('express-sslify');
 const admin = require('firebase-admin');
+
+require('dotenv').config({ path: './.env' });
+const {
+    NODE_ENV,
+    STRIPE_SECRET_KEY,
+    TWILIO_ACCOUNT_SID,
+    TWILIO_AUTH_TOKEN,
+    VERIFICATION_SID,
+    PORT,
+} = process.env;
+
+// Twilio Integration
+
+const twilio = require('twilio')(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
+
+// Firebase Service Account Info
 const ServiceAccount = require('./serviceAccountInfo.json');
 
-if (process.env.NODE_ENV !== 'production') require('dotenv').config();
+if (NODE_ENV !== 'production') require('dotenv').config();
 
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const stripe = require('stripe')(STRIPE_SECRET_KEY);
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = PORT || 5000;
 
-app.use(compression());
+// app.use(compression());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(enforce.HTTPS({ trustProtoHeader: true }));
+// app.use(enforce.HTTPS({ trustProtoHeader: true }));
 app.use(cors());
 
 admin.initializeApp({
     credential: admin.credential.cert(ServiceAccount),
 });
 
-const firestore = admin.firestore();
+// const firestore = admin.firestore();
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, 'client/build')));
@@ -57,4 +73,42 @@ app.post('/payment', (req, res) => {
             res.status(200).send({ success: stripeRes, userRes: currentUser });
         }
     });
+});
+
+app.post('/phone-verify-send', async (req, res) => {
+    const channel = 'sms';
+    const { phoneNumber } = req.body;
+    let verificationRequest;
+
+    try {
+        verificationRequest = await twilio.verify
+            .services(VERIFICATION_SID)
+            .verifications.create({ to: phoneNumber, channel });
+        console.log(verificationRequest);
+    } catch (e) {
+        console.error(e);
+        res.status(500).json({ error: 'ERROR', desc: e });
+    }
+});
+
+app.post('/phone-verify-check', async (req, res) => {
+    const { code, phoneNumber } = req.body;
+    let verificationResult;
+
+    try {
+        verificationResult = await twilio.verify
+            .services(VERIFICATION_SID)
+            .verificationChecks.create({ code, to: phoneNumber });
+    } catch (e) {
+        logger.error(e);
+        return res.status(500).send(e);
+    }
+
+    console.log(verificationResult);
+
+    if (verificationResult.status === 'approved') {
+        return res.status(200).json({ success: true });
+    } else {
+        return res.status(500).send('already verified');
+    }
 });
