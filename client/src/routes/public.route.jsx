1e3e64e6f152a60fa63ffaa8c161ba1e34727217
Muhammad Ali Kazmi <ali.smak099@gmail.com>
2020-06-09 23:51:57 +0500

    Develop public route with conditional routing

---
+++ client/src/routes/public.route.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+const PublicRoute = ({
+    component: Component,
+    isAuthenticated,
+    currentUser,
+    isAuthPage,
+    ...rest
+}) => {
+    if (isAuthenticated) {
+        if (currentUser.phoneNumberVerified === false) {
+            return <Redirect to='/signup-verification' />;
+        }
+
+        if (isAuthPage) {
+            return <Redirect to='/' />;
+        }
+    }
+
+    return <Route {...rest} render={(props) => <Component {...props} />} />;
+};
+
+const mapStateToProps = (state) => ({
+    isAuthenticated: state.user.isAuthenticated,
+    currentUser: state.user.currentUser,
+});
+
+export default connect(mapStateToProps)(PublicRoute);
